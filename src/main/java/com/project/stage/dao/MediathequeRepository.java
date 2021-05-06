79e79a74440b04e36c3a30228ed79a8b536951f7
patrick Tchounda <tchoupalan@gmail.com>
2021-05-06 19:47:45 +0200

    dependence postgres pour pouvoir se pluger aux bdd postgres

--- src/main/java/com/project/stage/dao/MediathequeRepository.java
+++ src/main/java/com/project/stage/dao/MediathequeRepository.java
@@ -1,7 +1,9 @@
 package com.project.stage.dao;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +15,13 @@ public interface MediathequeRepository extends JpaRepository<Media, Long>{
 	
 	
 	@Query("SELECT m FROM Media m  WHERE m.title like %:pTitle%")
-	List<Media> findByTitle(@Param("pTitle") String title); 
+	List<Media> findByTitle(@Param("pTitle") String title);
+
+	Set<Media> findMediaByTitleLike(String title);
+
+	Optional<Media> findMediaById(Long id);
+
+	Set<Media> findMediaByDescriptionLike(String description);
 	
 
 }
