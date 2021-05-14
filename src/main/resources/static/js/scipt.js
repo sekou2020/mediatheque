296fdc370f06c760d671912db86acf5d7f5529b3
patrick Tchounda <tchoupalan@gmail.com>
2021-05-14 15:20:53 +0200

    optimisation fonction open modal

--- src/main/resources/static/js/scipt.js
+++ src/main/resources/static/js/scipt.js
@@ -5,93 +5,45 @@
 function afficherPopupConfirmationLien(question, lien) {
 
     // crée la division qui sera convertie en popup
-
     $('body').append('<div id="popupconfirmation" title="Confirmation"></div>');
-
     $("#popupconfirmation").html(question);
 
- 
-
     // transforme la division en popup
-
     var popup = $("#popupconfirmation").dialog({
-
         autoOpen: true,
-
         width: 400,
-
         dialogClass: 'dialogstyleperso',
-
         hide: "fade",
-
         buttons: [
-
             {
-
                 text: "Oui",
-
                 class: "ui-state-question",
-
                 click: function () {
-
                     $(this).dialog("close");
-
                     var hrefdefini = false;
-
-                    
-
                     if (lien != null) {
-
                         if ($(lien).attr("href") != undefined) {
-
                             hrefdefini = true;
-
                             // affiche la page de l'attribut href
-
                             window.location.href = $(lien).attr("href");
-
                         }
-
                     }
-
-               
-
                     if (!hrefdefini) {
-
                         // réaffiche la page actuelle
-
                         window.location.reload();
-
                     }
-
- 
-
                     $("#popupconfirmation").remove();
-
- 
-
                 }
-
             },
-
             {
-
                 text: "Non",
-
                 class: "ui-state-question",
-
                 click: function () {
-
                     $(this).dialog("close");
-
                     $("#popupconfirmation").remove();
-
                 }
-
             }
-
         ]
-
     });
 
  
